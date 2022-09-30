e560342ece7b5f14f1919c76e7e110877d7bab92
Daniel <daniel.dreiling@gmx.de>
2022-09-30 21:05:13 +0200

    fixed issue with 0 words in line

--- Aaleren/LineInterpreter.cs
+++ Aaleren/LineInterpreter.cs
@@ -25,17 +25,18 @@ namespace Aaleren
         private void ManageConditions(int index)
         {
             //Line is empty
-            if(!FirstWordIsAal(index) && emptyLines % 5 == 0 && emptyLines > 0)
+            if(!LineIsEmpty(index) && emptyLines % 5 == 0 && emptyLines > 0)
             {
-                variables[0]++;
-                Console.WriteLine(variables[0]);
+                variables[variableIndex]++;
+                Console.WriteLine(variables[variableIndex]);
             }
         }
-        private bool FirstWordIsAal(int index)
+        private bool LineIsEmpty(int index)
         {
             string[] wordsInLine = Editor.GetWordsInLine(index);
-            if (wordsInLine[0] == "")
+            if (wordsInLine[0] == "" && Editor.GetWordsInLine(index).Length == 1)
             {
+                Console.WriteLine($"wordsInLine {index}: {Editor.GetWordsInLine(index).Length}");
                 emptyLines++;
                 return false;
             }
@@ -56,8 +57,19 @@ namespace Aaleren
                 variableIndex++;
             }
         }
+        private void IncrementVariableIndex(int count)
+        {
+            if (variableIndex + count >= 10)
+            {
+                variableIndex = 0;
+            }
+            else
+            {
+                variableIndex += count;
+            }
+        }
 
-        
+
         /*
          * Wenn eine Zeile im Text leer ist / Empty space in the editor
                 if(Editor.GetLines()[i].Length == 0)
